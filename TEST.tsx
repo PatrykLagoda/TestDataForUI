4f23a170c41ae2f7ea9513f6c8323fcaa9db7023
PatrykLagoda <patryck.lagoda@brightest.be>
2025-04-07 14:22:59 +0200

    test3

--- TEST.tsx
+++ TEST.tsx
@@ -1,67 +1,43 @@
 import { Wait } from '../../../src/common/index.js';
-import { Browser, WebButton, WebElementArray, WebLabel, WebTextfield } from '../../../src/web/index.js';
+import { WebButton, WebTextfield, WebElementArray, WebLabel } from '../../../src/web/index.js';
+import { DEFAULT_TIMEOUT } from '../configs/e2eConstants';
 import BaseWikipediaPage from './baseWikipediaPage.js';
 
-const PAGE_PATH = '/Main_Page';
+const SCREEN_SELECTOR = '#searchInput';
 
 class MainPage extends BaseWikipediaPage {
-    constructor(browser: Browser) {
-        super(browser, 'main', PAGE_PATH);
+    constructor() {
+        super(null, 'main', '/Main_Page'); // Adjust if 'browser' is required
     }
 
-    // Private elements using class field style
+    // Make it private so people can't mess with it
+    // Source: https://github.com/tc39/proposal-class-fields#private-fields
+    get #screen() {
+        return $(SCREEN_SELECTOR);
+    }
 
-    /**
-     * @returns {WebButton} Search bar icon button
-     */
     get #searchBarIconButton() {
         return new WebButton('.mw-ui-icon-search', 'search button', this);
     }
 
-    /**
-     * @returns {WebTextfield} Main search bar
-     */
     get #searchBar() {
         return new WebTextfield('#searchInput', 'search bar', this);
     }
 
-    /**
-     * @returns {WebButton} Search confirmation button
-     */
     get #searchButton() {
         return new WebButton('.cdx-search-input__end-button', 'search', this);
     }
 
-    /**
-     * @returns {WebElementArray<WebLabel>} List of top search results
-     */
     get #topSearchResults() {
         return new WebElementArray<WebLabel>('.cdx-search-result-title', 'search result', this);
     }
 
     /**
-     * Click the search confirmation button
-     */
-    async clickSearch() {
-        await this.#searchButton.click();
-    }
-
-    /**
-     * Get top search results as an array of strings
-     *
-     * @returns {Promise<string[]>}
-     */
-    async getTopResultsAsStrings() {
-        const elements = await this.#topSearchResults.getElements();
-        return Promise.all(elements.map(el => el.getText()));
-    }
-
-    /**
-     * Enter a search term into the search bar
+     * Enter a term in the search bar and optionally trigger the search
      *
      * @param {string} searchTerm
      */
-    async enterSearchTerm(searchTerm: string) {
+    async enterSearchTerm(searchTerm) {
         try {
             await Wait.until(async () => {
                 return (await this.#searchBarIconButton.isDisplayed(true)) === true;
@@ -73,6 +49,23 @@ class MainPage extends BaseWikipediaPage {
         await this.#searchBar.sendText(searchTerm);
         await Wait.for(500);
     }
+
+    /**
+     * Click the search submit button
+     */
+    async clickSearch() {
+        await this.#searchButton.click();
+    }
+
+    /**
+     * Get an array of strings for the top search result titles
+     *
+     * @returns {Promise<string[]>}
+     */
+    async getTopResultsAsStrings() {
+        const elements = await this.#topSearchResults.getElements();
+        return Promise.all(elements.map(el => el.getText()));
+    }
 }
 
-export default MainPage;
+export default new MainPage();
