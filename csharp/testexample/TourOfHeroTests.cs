d32239a92e29c803a919ed77328ea439ee574dba
PatrykLagoda <patryck.lagoda@brightest.be>
2025-04-10 10:33:23 +0200

    Csharp extra$

---
+++ csharp/testexample/TourOfHeroTests.cs
@@ -0,0 +1,49 @@
+﻿using Brightest.Taf.DemoTests.TourOfHero.Pages;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Brightest.Taf.DemoTests.Tests
+{
+    [TestFixture]
+    public class TourOfHeroTests
+    {
+        [Test]
+        public void CanAddAHero()
+        {
+            // Arrange
+            var heroesView = new HeroesView();
+            var heroName = "Laurens";
+
+            // Act
+            heroesView.OpenPage();
+            heroesView.EnterHeroName(heroName);
+            heroesView.ClickAddHero();
+            var isHeroPresent = heroesView.IsHeroInList(heroName);
+            heroesView.ClosePage();
+
+            // Assert
+            isHeroPresent.Should().BeTrue();
+        }
+
+        [Test]
+        public void CanDeleteAHero()
+        {
+            // Arrange
+            var heroesView = new HeroesView();
+            var heroName = "Laurens";
+
+            // Act
+            heroesView.OpenPage();
+            heroesView.EnterHeroName(heroName);
+            heroesView.ClickAddHero();
+            var isHeroPresent = heroesView.IsHeroInList(heroName);
+            heroesView.DeleteHero(heroName);
+            var isHeroPresentDeleted = heroesView.IsHeroInList(heroName);
+            heroesView.ClosePage();
+
+            // Assert
+            isHeroPresent.Should().BeTrue();
+            isHeroPresentDeleted.Should().BeTrue();
+        }
+    }
+}
