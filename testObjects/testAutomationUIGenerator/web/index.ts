0a529e51f0b66156554e1c447d996b05d0190d3c
PatrykLagoda <patryck.lagoda@brightest.be>
2025-04-15 08:31:36 +0200

    Wrong file fix deletions

--- testObjects/testAutomationUIGenerator/web/index.ts
+++ testObjects/testAutomationUIGenerator/web/index.ts
@@ -1,6 +1,5 @@
 export * from "./mainPage.js";
 export * from "./loginPage.js";
-export * from "./resultPage.js";
 export * from "./repositoriesPage.js";
 export * from "./repositoryModal.js";
 export * from "./testAutomationUIGeneratorPages.js";
